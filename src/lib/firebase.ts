51ca45ff39c6eebc56eddfb39a0da7bf6400c09f
Gift Egbonyi. <egbonyigiftvicky@gmail.com>
2025-07-15 13:56:59 +0100

    Add fb envs.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,13 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDemo_apiKey_for_miiah_birthday",
-  authDomain: "miiah-birthday-demo.firebaseapp.com",
-  projectId: "miiah-birthday-demo",
-  storageBucket: "miiah-birthday-demo.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:demo_app_id"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
